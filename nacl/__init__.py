64d75740c485b8e3ac3108b916cdf72ad41b0611
Donald Stufft <donald.stufft@gmail.com>
2013-02-22 09:36:47 -0500

    Add meta information to the nacl package

--- nacl/__init__.py
+++ nacl/__init__.py
@@ -1,1 +1,13 @@
+from . import __about__
 from . import hash  # pylint: disable=W0622
+
+
+__all__ = ["hash"] + __about__.__all__
+
+
+# - Meta Information -
+# This is pretty ugly
+for attr in __about__.__all__:
+    if hasattr(__about__, attr):
+        globals()[attr] = getattr(__about__, attr)
+# - End Meta Information -
