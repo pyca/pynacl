e4d317395d3b0e249710f0d5b3a194e8ce905c10
Donald Stufft <donald.stufft@gmail.com>
2013-02-22 16:03:21 -0500

    Fix a missing .tar.gz

--- tests/install_sodium.sh
+++ tests/install_sodium.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd $HOME
 wget http://download.dnscrypt.org/libsodium/releases/libsodium-0.2.tar.gz
-tar xvf libsodium-0.2
+tar xvf libsodium-0.2.tar.gz
 cd libsodium-0.2
 ./configure --disable-debug --disable-dependency-tracking
 make
