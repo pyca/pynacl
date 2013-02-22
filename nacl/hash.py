30c4ef18e4bc8e8e0b374b97c52eaf71caf2ff9c
Donald Stufft <donald.stufft@gmail.com>
2013-02-22 00:42:28 -0500

    Add nacl.hash, a module for secure message digests
    * Supports sha256 and sha512

---
+++ nacl/hash.py
@@ -0,0 +1,26 @@
+import binascii
+
+from . import nacl
+from .exceptions import CryptoError
+
+
+def sha256(message, binary=False):
+    digest = nacl.ffi.new("unsigned char[]", nacl.lib.crypto_hash_sha256_BYTES)
+    if not nacl.lib.crypto_hash_sha256(digest, message, len(message)):
+        raise CryptoError("Hashing failed")
+    digest = nacl.ffi.string(digest)
+
+    if binary:
+        return digest
+    return binascii.hexlify(digest).decode("ascii")
+
+
+def sha512(message, binary=False):
+    digest = nacl.ffi.new("unsigned char[]", nacl.lib.crypto_hash_sha512_BYTES)
+    if not nacl.lib.crypto_hash_sha512(digest, message, len(message)):
+        raise CryptoError("Hashing failed")
+    digest = nacl.ffi.string(digest)
+
+    if binary:
+        return digest
+    return binascii.hexlify(digest).decode("ascii")
