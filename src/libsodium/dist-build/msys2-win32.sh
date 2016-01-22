97252fe8951b255b411e95aa1b30a875bebd0133
Paul Kehrer <paul.l.kehrer@gmail.com>
2016-01-22 15:08:17 -0600

    upgrade to libsodium 1.0.8

--- src/libsodium/dist-build/msys2-win32.sh
+++ src/libsodium/dist-build/msys2-win32.sh
@@ -3,6 +3,8 @@
 export CFLAGS="-O3 -fomit-frame-pointer -m32 -march=pentium2 -mtune=nocona"
 export PREFIX="$(pwd)/libsodium-win32"
 
+make distclean > /dev/null
+
 ./configure --prefix="$PREFIX" --exec-prefix="$PREFIX" \
             --host=i686-w64-mingw32 && \
 make && \
