0ffa480d675a0928a530de1e337c3ad42f483730
Lorenzo M. Catucci <lorenzo@sancho.ccd.uniroma2.it>
2017-01-27 21:07:59 +0100

    Install python 3.6 via pyenv

--- .travis/install.sh
+++ .travis/install.sh
@@ -51,4 +51,15 @@ if [[ "${TOXENV}" == "py33" ]]; then
     pyenv global 3.3.6
 fi
 
+if [[ "${TOXENV}" == "py36" ]]; then
+    rm -rf ~/.pyenv
+    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+    git -C  ~/.pyenv reset --hard ${PYENV_COMMIT:-HEAD}
+    PYENV_ROOT="$HOME/.pyenv"
+    PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+    pyenv install 3.6.0
+    pyenv global 3.6.0
+fi
+
 pip install -U tox coverage
