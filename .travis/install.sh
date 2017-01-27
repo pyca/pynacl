99e427ef22a0a3248a41e79d141d639b0e8c3add
Lorenzo M. Catucci <lorenzo@sancho.ccd.uniroma2.it>
2017-01-27 21:07:59 +0100

    Pin pyenv at the last known-working commit

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,14 @@
 set -e
 set -x
 
+# pin pyenv at the last known good release
+PYENV_COMMIT=99d16707e372143fb35d822c26fe8427719b903c
+
+# the following commit breaks tox virtualenv building
+# on travis-ci
+#
+# PYENV_COMMIT=2657f1049cd45656918f601096509957d5b74e7c
+
 if [[ $SODIUM_INSTALL == 'system' ]]; then
     wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
     tar zxvf LATEST.tar.gz
@@ -17,6 +25,7 @@ fi
 if [[ "${TOXENV}" == "pypy" ]]; then
     rm -rf ~/.pyenv
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+    git -C  ~/.pyenv reset --hard ${PYENV_COMMIT:-HEAD}
     PYENV_ROOT="$HOME/.pyenv"
     PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
@@ -27,6 +36,7 @@ fi
 if [[ "${TOXENV}" == "py26" ]]; then
     rm -rf ~/.pyenv
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+    git -C  ~/.pyenv reset --hard ${PYENV_COMMIT:-HEAD}
     PYENV_ROOT="$HOME/.pyenv"
     PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
@@ -37,6 +47,7 @@ fi
 if [[ "${TOXENV}" == "py33" ]]; then
     rm -rf ~/.pyenv
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+    git -C  ~/.pyenv reset --hard ${PYENV_COMMIT:-HEAD}
     PYENV_ROOT="$HOME/.pyenv"
     PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
