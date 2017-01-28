e202efb8f818f109c105c514fbbb622bb65dd191
lmctv <lorenzo@sancho.ccd.uniroma2.it>
2017-01-28 15:44:27 +0100

    Pin pyenv at the last known-working commit (#241)
    * Pin pyenv at the last known-working commit
    
    * Unpin version after upstream revert of the breaking change

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+# to pin pyenv version, set the PYENV_COMMIT variable
+# to the required version commit identifier/tag like in
+# PYENV_COMMIT=v1.0.7
+
 if [[ $SODIUM_INSTALL == 'system' ]]; then
     wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
     tar zxvf LATEST.tar.gz
@@ -17,6 +21,7 @@ fi
 if [[ "${TOXENV}" == "pypy" ]]; then
     rm -rf ~/.pyenv
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+    git -C  ~/.pyenv reset --hard ${PYENV_COMMIT:-HEAD}
     PYENV_ROOT="$HOME/.pyenv"
     PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
@@ -27,6 +32,7 @@ fi
 if [[ "${TOXENV}" == "py26" ]]; then
     rm -rf ~/.pyenv
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+    git -C  ~/.pyenv reset --hard ${PYENV_COMMIT:-HEAD}
     PYENV_ROOT="$HOME/.pyenv"
     PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
@@ -37,6 +43,7 @@ fi
 if [[ "${TOXENV}" == "py33" ]]; then
     rm -rf ~/.pyenv
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+    git -C  ~/.pyenv reset --hard ${PYENV_COMMIT:-HEAD}
     PYENV_ROOT="$HOME/.pyenv"
     PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
