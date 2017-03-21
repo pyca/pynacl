04b4dec5d32d8adf806fc3425c34eaeb0413725a
Lorenzo M. Catucci <lorenzo@sancho.ccd.uniroma2.it>
2017-03-21 21:38:37 +0100

    Add a workaround for libsodium's strangeness

--- src/nacl/bindings/crypto_pwhash.py
+++ src/nacl/bindings/crypto_pwhash.py
@@ -57,9 +57,9 @@ crypto_pwhash_argon2i_MEMLIMIT_SENSITIVE = \
 crypto_pwhash_argon2i_MEMLIMIT_MIN = \
     max(lib.crypto_pwhash_argon2i_memlimit_min(), 8*1024)
 # Workaround for jedisct1/libsodium#516
-
 crypto_pwhash_argon2i_MEMLIMIT_MAX = \
-    lib.crypto_pwhash_argon2i_memlimit_max()
+    max(lib.crypto_pwhash_argon2i_memlimit_max(), 128 * 1024 * 1024)
+# Workaround for too small MEMLIMIT on 32 bit windows
 crypto_pwhash_argon2i_OPSLIMIT_MIN = \
     lib.crypto_pwhash_argon2i_opslimit_min()
 crypto_pwhash_argon2i_OPSLIMIT_MAX = \
