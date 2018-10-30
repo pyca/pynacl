d68b3471c54086e66a033d9ddf4b8e66978fa8ed
Stefano Palazzo <stefano.palazzo@gmail.com>
2018-10-30 18:43:11 +0100

    Cleaned up headers

--- src/bindings/crypto_sign.h
+++ src/bindings/crypto_sign.h
@@ -33,12 +33,6 @@ int crypto_sign_open(unsigned char *m,  unsigned long long *mlen,
                const unsigned char *sm, unsigned long long smlen,
                const unsigned char *pk);
 
-int crypto_sign_ed25519_sk_to_pk(unsigned char *pk,
-                                 const unsigned char *sk);
-
-int crypto_sign_ed25519_sk_to_seed(unsigned char *pk,
-                                 const unsigned char *seed);
-
 int crypto_sign_ed25519_pk_to_curve25519(unsigned char *curve25519_pk,
                                          const unsigned char *ed25519_pk);
 
