2fc1d522ab3e66a13e61d6aaf8d5df654829cd02
kubinski <ryankubinski@gmail.com>
2018-12-24 03:21:01 -0700

    Update setup_requirements to match pyproject.toml (#485)
    Updated setup_requirements to match the requirements listed in pyproject.toml.

--- setup.py
+++ setup.py
@@ -34,7 +34,8 @@ except ImportError:
 
 
 requirements = ["six"]
-setup_requirements = []
+setup_requirements = ["setuptools",
+                      "wheel"]
 test_requirements = ["pytest>=3.2.1,!=3.3.0",
                      "hypothesis>=3.27.0"]
 docs_requirements = ["sphinx>=1.6.5",
