4a3c081b490823781c6360179bd858dba856b148
Lorenzo M. Catucci <lorenzo@sancho.ccd.uniroma2.it>
2019-01-18 19:57:05 +0100

    Update copyright year

--- src/nacl/utils.py
+++ src/nacl/utils.py
@@ -1,4 +1,4 @@
-# Copyright 2013 Donald Stufft and individual contributors
+# Copyright 2013-2019 Donald Stufft and individual contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
