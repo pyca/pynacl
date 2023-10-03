d34f46b6b3b258795a12002b8dbac7cb011a36a9
Alex Gaynor <alex.gaynor@gmail.com>
2023-10-03 07:15:32 -0400

    Refresh CI for 3.12

--- setup.py
+++ setup.py
@@ -231,5 +231,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
