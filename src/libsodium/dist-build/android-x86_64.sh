42108622ed17726c4c6e71b707c46d439ed6054e
Paul Kehrer <paul.l.kehrer@gmail.com>
2025-04-21 15:27:07 -0700

    bump to libsodium-1.0.20-stable (03-Apr-2025)
    This also adds arm64 windows builds to the msvc zip

--- src/libsodium/dist-build/android-x86_64.sh
+++ src/libsodium/dist-build/android-x86_64.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export TARGET_ARCH=westmere
 export CFLAGS="-Os -march=${TARGET_ARCH}"
-NDK_PLATFORM=android-21 ARCH=x86_64 HOST_COMPILER=x86_64-linux-android "$(dirname "$0")/android-build.sh"
+export LDFLAGS="-Wl,-z,max-page-size=16384"
+ARCH=x86_64 HOST_COMPILER=x86_64-linux-android "$(dirname "$0")/android-build.sh"
