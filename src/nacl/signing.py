d56989af7feca49d81dde4e715ef0a5abe5dadb0
Paul Kehrer <paul.l.kehrer@gmail.com>
2025-09-10 15:35:46 +0900

    fix doc mistake

--- src/nacl/signing.py
+++ src/nacl/signing.py
@@ -104,7 +104,7 @@ class VerifyKey(encoding.Encodable, StringFixer):
         """
         Verifies the signature of a signed message, returning the message
         if it has not been tampered with else raising
-        :class:`~nacl.signing.BadSignatureError`.
+        :class:`~nacl.exceptions.BadSignatureError`.
 
         :param smessage: [:class:`bytes`] Either the original messaged or a
             signature and message concated together.
