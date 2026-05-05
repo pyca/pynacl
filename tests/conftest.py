39117100f8de6f71954f77827e0c5608e6a28836
greateggsgreg <36009512+greateggsgreg@users.noreply.github.com>
2026-05-05 19:40:31 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,5 @@ def pytest_runtest_setup(item: pytest.Item) -> None:
     if sys.platform == "emscripten":
         for marker in item.iter_markers(name="skip_emscripten"):
             pytest.skip(
-                marker.kwargs.get(
-                    "reason", "Skipped under Emscripten/Pyodide"
-                )
+                marker.kwargs.get("reason", "Skipped under Emscripten/Pyodide")
             )
